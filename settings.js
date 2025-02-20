1d98681a35842eb2236de0d03fb45994e441f905
Sl madhuwa <88928383+slmadhuwa@users.noreply.github.com>
2025-02-20 18:05:15 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "SACHI & MADHU BOT",
+  botOwner: 'sl.madhuwa', // Your name
+  ownerNumber: '94742274409', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
